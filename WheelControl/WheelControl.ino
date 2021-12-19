9c1509a355341ff62aae5162a17de7a1cbc51e15
Luis <some@email.com>
2021-12-18 16:32:44 -0800

    Calibrations and Functions for Motors made

--- WheelControl/WheelControl.ino
+++ WheelControl/WheelControl.ino
@@ -1,4 +1,6 @@
 int ENA = 5, ENB = 6, IN1 = 7, IN2 = 8, IN3 = 9, IN4 = 11;
+float distance = 0;
+int degRotate = 0;
 
 void setup() {
   pinMode(ENA, OUTPUT);
@@ -13,12 +15,60 @@ void setup() {
 }
 
 void loop() {
-  //Move Forward
+ 
+  rightTurn(90);
+  while(1);
+ 
+  
+}
+
+void forward(float distance){
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
-  delay(1000);
-  
-  
+  //calibration from distance to time in ms 
+  float t = (distance / 2.45) * 1000;
+  delay(t);
+  stopCar();
+}  
+
+void backward(float distance){
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  //calibration from distance to time in ms
+  float t = (distance / 2.45) * 1000;
+  delay(t);
+  stopCar();
 }
+
+void rightTurn(int deg){  
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  //Calibrate from degrees to time in ms (Theta = omega * t)
+  float t = (deg / 280.0) * 1000;
+  delay(t);
+  stopCar();
+}
+
+void leftTurn(int deg){
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  //Calibrate from degrees to time in ms (Theta = omega * t)
+  float t = (deg / 260.0) * 1000;
+  delay(t);
+  stopCar();
+}  
+
+void stopCar(){
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+}  
