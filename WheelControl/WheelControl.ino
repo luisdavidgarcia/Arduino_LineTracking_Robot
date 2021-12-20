c42468249e238a332a88a7d995ed91e3fadf9d4c
Luis <some@email.com>
2021-12-19 19:10:09 -0800

    Modified Turn Functions Due to new input of Speed, but also have to redo calibrations due to faulty wheel that is now fixed

--- WheelControl/WheelControl.ino
+++ WheelControl/WheelControl.ino
@@ -16,11 +16,20 @@ void setup() {
 
 void loop() {
 
-  float velocity = 2;
+  float velocity = 1.2;
   float analogWheelValue = (velocity - 0.35)/.0075;
-
   inputSpeed(analogWheelValue,analogWheelValue);
-  forward(5,velocity);
+  rightTurn(180,analogWheelValue);
+  //forward(5,velocity);
+
+  //velocity = 1.5;
+  //analogWheelValue = (velocity - 0.35)/.0075;
+  //inputSpeed(analogWheelValue,analogWheelValue);
+  //backward(5,velocity);
+
+ // inputSpeed(255,255);
+ //calL(analogWheelValue);
+  
   while(1);
   
 }
@@ -45,37 +54,47 @@ void forward(float distance, float velocity){
   stopCar();
 }  
 
-void backward(float distance, float wheelValue){
+void backward(float distance, float velocity){
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
   digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-  //Calculate Velocity for Forward and backward by Using point slope form equation for points at 130 and 255
-  float velocity = .0075*wheelValue + .35;
+  digitalWrite(IN4,LOW); 
   //calibration from distance to time in ms
   float t = (distance / velocity) * 1000;
   delay(t);
   stopCar();
 }
 
-void rightTurn(int deg){  
+void rightTurn(int deg, float analogWheelValue){  
+  //stop car and delay it to ensure turn occurs smoothly 
+  stopCar();
+  delay(100);
+  //analog value should be smaller for smoother turn
+  analogWrite(ENA,130);
+  analogWrite(ENB,130); 
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
   digitalWrite(IN3,HIGH);
   digitalWrite(IN4,LOW);
   //Calibrate from degrees to time in ms (Theta = omega * t)
-  float t = (deg / 280.0) * 1000;
+  float t = ((deg + 36)/119.143) * 1000;
   delay(t);
   stopCar();
 }
 
-void leftTurn(int deg){
+void leftTurn(int deg, float analogWheelValue){
+  //stop car and delay it to ensure turn occurs smoothly 
+  stopCar();
+  delay(100);
+  //analog value should be smaller for smoother turn
+  analogWrite(ENA,130);
+  analogWrite(ENB,130); 
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
-  //Calibrate from degrees to time in ms (Theta = omega * t)
-  float t = (deg / 260.0) * 1000;
+  //Calibrate from degrees to time in ms (Theta = omega * t) using Linear Regression
+  float t = ((deg + 36)/119.143) * 1000;
   delay(t);
   stopCar();
 }  
@@ -86,3 +105,20 @@ void stopCar(){
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,LOW);
 }  
+
+
+//Calibration Functions
+void calL(int analogWheelValue){
+  stopCar();
+  delay(100);
+  analogWrite(ENA,130);
+  analogWrite(ENB,130);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  delay(3000);
+  analogWrite(ENA,analogWheelValue);
+  analogWrite(ENB,analogWheelValue); 
+  stopCar();
+}
